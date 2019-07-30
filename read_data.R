6de3804d9cbb2c7b8b23ab2c5b3b06bbe4ac1e31
nilsreimer <31796597+nilsreimer@users.noreply.github.com>
2019-07-30 18:03:16 +0100

    revision

---
+++ read_data.R
@@ -0,0 +1,26 @@
+rm(list = ls())
+
+# Notes -------------------------------------------------------------------
+  
+  #########################################################################
+  # d1 contains participants' responses from the first part of the study  #
+  # (i.e. the triple crossed-categorization task); d2 contains responses  #
+  # from the second part of the study (i.e. the questionnaire). For item  #
+  # wordings, see materials/materials.pdf.                                #
+  #########################################################################
+
+# Load packages -----------------------------------------------------------
+  library(tidyverse)
+
+
+# Import data -------------------------------------------------------------
+  d1 <- read_rds("data/d1.rds")
+  d2 <- read_rds("data/d2.rds")
+
+
+# Analyze -----------------------------------------------------------------
+
+  #########################################################################
+  # Start your own analyses here.                                         #
+  #########################################################################
+  
